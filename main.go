8b9ac9dd06a0f6cb1655a480e8acaf57adad8614
cooperbraun13 <cooperjbraun13@gmail.com>
2026-01-11 02:44:27 -0800

    feat: implemented update method which is called when 'stuff happens'. input from the user determines what the update command does

--- main.go
+++ main.go
@@ -26,3 +26,45 @@ func (m model) Init() tea.Cmd {
 	// just return `nil`, which means 'no I/O right now, please'
 	return nil
 }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// is it a key press?
+	case tea.KeyMsg:
+
+		// cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// these keys should exit the program
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// the "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// the "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// the "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// return the updated model to the Bubble Tea runtime for processing
+	// note that we're not returning a command
+	return m, nil
+}

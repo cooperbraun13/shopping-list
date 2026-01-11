8e98d5317bf33829bd542d40aad6ff0f0371360d
cooperbraun13 <cooperjbraun13@gmail.com>
2026-01-11 02:33:28 -0800

    feat: setup file and defined our model along with initializing the applications state and introducing a init method for i/o (which wont be used, at least for now...)

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices  []string         // items on the to-do list
+	cursor   int              // which to-do list item our cursor is pointing at
+	selected map[int]struct{} // which to-do items are selected
+}
+
+func initialModel() model {
+	return model{
+		// our to-do list is a grocery list
+		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+
+		// a map which indicates which choices are selected. we're using
+		// the map like a mathematical set. the keys refer to the indexes
+		// of the `choices` slice, above
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// just return `nil`, which means 'no I/O right now, please'
+	return nil
+}
